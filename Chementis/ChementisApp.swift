c1d2714d8354b67ef00aac5933bfd1de28cc9c57
hunter <hdowneymac@gmail.com>
2023-04-07 14:55:25 -0500

    Initial Commit

---
+++ Chementis/ChementisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChementisApp.swift
+//  Chementis
+//
+//  Created by hunter downey on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ChementisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
